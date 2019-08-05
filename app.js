2af06a1c59f905e0827e3fa720c546d49d2911d3
SantiagoFernandezdev <velascosantiago28@gmail.com>
2019-08-04 22:26:43 -0500

    dfdf

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, 'www')));
 app.set('port', process.env.PORT || 3000);
 
+app.all ( '*' , (req, res) => {  
+    res.status(200).sendFile (__ dirname +  '/www/index.html' );  
+});  
+
 app.listen(app.get('port'), () => {
     console.log(`Corriendo aplicacion en el purto ${app.get('port')}`)
 })
